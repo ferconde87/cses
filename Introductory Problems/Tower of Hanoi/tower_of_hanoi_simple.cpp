60b52ac7eb960310a44e967dbb0323f343de1f6f
fer <ferconde87@gmail.com>
2021-03-28 22:30:26 +0700

    Tower of Hanoi

--- Introductory Problems/Tower of Hanoi/tower_of_hanoi_simple.cpp
+++ Introductory Problems/Tower of Hanoi/tower_of_hanoi_simple.cpp
@@ -4,23 +4,26 @@ using namespace std;
  int result = 0;
  vector<pair<int, int> > v;
 
-void towerOfHanoi(int n, int from, int to, int aux, int moves) {
+void towerOfHanoi(int n, int from, int to, int aux) {
   if (n == 1){
+    result++;
     v.push_back(make_pair(from, to));
     return;
   }
-  result++;
-  towerOfHanoi(n - 1, from, aux, to, moves+1);
+  
+  towerOfHanoi(n - 1, from, aux, to);
+  
   v.push_back(make_pair(from, to));
   result++;
-  towerOfHanoi(n - 1, aux, to, from, moves+1);
+  
+  towerOfHanoi(n - 1, aux, to, from);
 }
  
 int main() {
   int n;
   cin >> n,
-  towerOfHanoi(n, 1, 3, 2, 0); 
-  cout << result+1 << endl;
+  towerOfHanoi(n, 1, 3, 2); 
+  cout << result << endl;
   for(auto p : v){
     cout << p.first << " " << p.second << endl;
   }
