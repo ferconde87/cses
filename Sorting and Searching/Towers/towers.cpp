39ae3a06080917bdd5ad58e27e13aca7f73a1eb7
fer <ferconde87@gmail.com>
2021-04-05 16:03:39 +0700

    Towers

---
+++ Sorting and Searching/Towers/towers.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+#define cn(x) cout << #x << " = " << x << endl;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  multiset<int> s;
+  int ans = 0;
+  for(int i = 0; i<n; ++i){
+    int x;
+    cin >> x;
+    auto it = s.upper_bound(x);
+    if(it == s.end()){
+      ans++;
+    }else{
+      s.erase(it);
+    }
+    s.insert(x);
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
