7f54676c848b6cad81c8385fae4aeff54f155f80
fer <ferconde87@gmail.com>
2021-04-11 10:32:31 +0700

    Removing Digits

---
+++ Dynamic Programming/Removing Digits/removing_digits.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for(int i = 10, j = 0; i <= 1000000; i*=10, ++j) v[j] = i;
+  int ans = 0;
+  int max = 0;
+  while(n){
+    int m = n;
+    for(int i = 5; i >= 0; --i){
+      int cur = m / v[i];
+      if(cur > max){
+        max = cur;
+      }
+      m -= cur*v[i];
+    }
+    int rem = m % 10;
+    if(rem > max) max = rem;
+    n-= max;
+    ans++;
+    max = 0;
+  }
+  cout << ans << endl;
+}
