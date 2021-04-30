aea3648d55e0e1e54d71ec95452b3c193f23047d
fer <ferconde87@gmail.com>
2021-04-30 18:15:54 +0700

    Coin Combinations II

--- Dynamic Programming/Coin Combinations II/c_c.cpp
+++ Dynamic Programming/Coin Combinations II/c_c.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int const lim = 1e9+7;
+int const N = 1e6+1;
+int dp[N];
 
 int main(){
   int n, target;
   cin >> n >> target;
   int coins[n];
   for(int i=0; i<n; ++i) cin >> coins[i];
-  sort(coins,coins+n);
-  int dp[target+1];
-  for(int i=1; i<=target; ++i) dp[i]=0;
   dp[0]=1;
   for(int c : coins){
     for(int x=1; x<=target; ++x){
