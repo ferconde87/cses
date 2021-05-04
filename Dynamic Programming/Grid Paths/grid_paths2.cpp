a4222d751b4a4591233bf5e2bcbfe8e9f911fe61
fer <ferconde87@gmail.com>
2021-05-04 13:46:13 +0700

    Grid Paths

--- Dynamic Programming/Grid Paths/grid_paths2.cpp
+++ Dynamic Programming/Grid Paths/grid_paths2.cpp
@@ -10,8 +10,9 @@ void grid_paths(vector<string> & matrix, int n){
   memo[0][0] = 1;
   for(int i = 0; i < n; ++i){
     for(int j = 0; j < n; ++j){
-      if(i > 0 && matrix[i-1][j] != '*') memo[i][j] += memo[i-1][j];
-      if(j > 0 && matrix[i][j-1] != '*') memo[i][j] += memo[i][j-1];
+      if(matrix[i][j]=='*') continue;
+      if(i) memo[i][j] += memo[i-1][j];
+      if(j) memo[i][j] += memo[i][j-1];
       memo[i][j] = memo[i][j] % mod;
     }
   }
@@ -23,7 +24,6 @@ int main(){
   for(int i = 0; i < n; ++i){
     cin >> matrix[i];
   }
-
-  if(matrix[n-1][n-1] != '*') grid_paths(matrix, n);
+  if(matrix[0][0] != '*' && matrix[n-1][n-1] != '*') grid_paths(matrix, n);
   cout << memo[n-1][n-1] << endl;
 }
