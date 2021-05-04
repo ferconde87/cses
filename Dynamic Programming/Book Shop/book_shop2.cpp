560dbd74f28255ac7af6783dff0c283a94a9343e
fer <ferconde87@gmail.com>
2021-05-04 14:59:25 +0700

    Book Shop

--- Dynamic Programming/Book Shop/book_shop2.cpp
+++ Dynamic Programming/Book Shop/book_shop2.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+
+
 #define ar array
 
 int main(){
@@ -12,12 +14,14 @@ int main(){
   for(int i = 1; i <= n; ++i) cin >> p[i][0];
   for(int i = 1; i <= n; ++i) cin >> p[i][1];
   vector<int> best(target+1, 0);
+  vector<int> last(target+1, 0);
 
   for(int i = 1; i <= n; ++i){
-    for(int x = target; x >= p[i][0]; --x){
-      best[x] = max(best[x], best[x-p[i][0]]+p[i][1]);
+    for(int x = p[i][0]; x <= target ; ++x){
+      best[x] = max(best[x], last[x-p[i][0]]+p[i][1]);
     }
+    last = best;
   }
-
+  
   cout << best[target] << endl;
 }
