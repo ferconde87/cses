9ae4907482ceb738d54d5c51246039684c939d2d
fer <ferconde87@gmail.com>
2021-05-06 17:46:10 +0700

    Removing Digits

--- Dynamic Programming/Removing Digits/removing_digits2.cpp
+++ Dynamic Programming/Removing Digits/removing_digits2.cpp
@@ -6,8 +6,8 @@ int main(){
   int n;
   cin >> n;
   int ans = 0;
-  int max_value = 0;
   while(n){
+    int max_value = 0;
     int m = n;
     while(m){
       max_value = max(max_value,m%10);
@@ -15,7 +15,6 @@ int main(){
     }
     n-= max_value;
     ans++;
-    max_value = 0;
   }
   cout << ans << endl;
 }
